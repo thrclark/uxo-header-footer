0c3955d9deba3e121b29b83d8ba2b8101cf580be
Tom <thrclark@indiana.edu>
2016-09-12 11:00:30 -0400

    verification complete screen

---
+++ php/verify-completed.php
@@ -0,0 +1,31 @@
+<?php $page_title = "Home" ?>
+
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+<?php include ('includes/head-meta.php') ?>
+<?php include ('includes/styles.php') ?>
+<?php include ('includes/scripts.php') ?>
+</head>
+<body id="top" >
+<?php include ('includes/brand-header2.php') ?>
+<?php include ('includes/header2.php') ?>
+<div class="main-content container" role="main">
+    <div id="main-content">
+        <?php include ('includes/instructions-top-completed.php') ?>
+    </div>
+</div>
+<?php include ('includes/brand-footer.php') ?>
+<script>
+    $(document).ready(function() {
+
+        $('.nav-tabs-responsive').tabCollapse();
+		$('.nav-tabs-responsive').on('shown-tabs.bs.tabcollapse', function(){
+    		$('.nav-tabs-responsive a:first').tab('show')
+});
+
+    });
+	
+</script>
+</body>
+</html>
