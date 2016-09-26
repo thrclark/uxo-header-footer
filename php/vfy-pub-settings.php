c226ca2e3446bd810e7b9981b65ff473f8b3d8d7
thrclark <thrclark@toms-mbp.attlocal.net>
2016-09-26 08:16:38 -0400

    starter pager for settings

---
+++ php/vfy-pub-settings.php
@@ -0,0 +1,34 @@
+<?php $page_title = "settings" ?>
+<!DOCTYPE html>
+<html lang=en-us>
+<head>
+<?php include ('includes/head-meta.php') ?>
+<?php include ('includes/styles-publishing.php') ?>
+<?php include ('includes/scripts.php') ?>
+</head>
+<body class="view-admin" id="top">
+<?php include ('includes/brand-header2.php') ?>
+<?php include ('includes/pub-header.php') ?>
+<div class="main-content container" role=main>
+    <div id="main-content">
+        <div class="row">
+            <div class="col-md-3">
+                <?php include ('includes/pub-nav.php') ?>
+            </div>
+            <div class="col-md-9">
+                <div class="primary-content">
+                    <section style="display:;" id="all_verifications">
+                        <div class="row section-head">
+                            <div class="col-md-12 section-header">
+                                <h3>General Settings</h3>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include ('includes/brand-footer.php') ?>
+</body>
+</html>
