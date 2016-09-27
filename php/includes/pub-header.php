e9cdbb3b1fd05db51953316e9dbee41f03618675
thrclark <thrclark@toms-mbp.attlocal.net>
2016-09-27 16:45:53 -0400

    Responsive fixes

--- php/includes/pub-header.php
+++ php/includes/pub-header.php
@@ -1,14 +1,13 @@
-<header style="min-height:100px">
+<header>
     <div class="container">
         <div class="row">
-            <div class="col-md-3">
-                <h1 class="navbar-brand" style="font-size: 44px;width: 100%;margin-top: 10px;">IU Verify</h1>
-                <div class="small" style="color:#ffffff">Verification Publishing</div>
+            <div class="col-xs-6">
+                <h1 class="navbar-brand">IU Verify</h1>
+                <div class="sub-header small">Verification Publishing</div>
             </div>
-            <div class="col-md-9"> 
-                <!-- <p class="navbar-text" style="color:#E4E4E4;margin-top: 26px;"><i class="fa fa-user"></i> <span class="sr-only">User: </span>thrclark</p>-->
+            <div class="col-xs-6">
                 <div class="dropdown navbar-text">
-                    <button id="dLabel" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"  class="btn btn-link" style="color:#e4e4e4" >Hello, Tom <span class="caret"></span> </button>
+                    <button id="dLabel" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"  class="btn btn-link" >Hello, Tom <span class="caret"></span> </button>
                     <ul class="dropdown-menu" aria-labelledby="dLabel">
                         <li><a href="#">Action</a></li>
                         <li><a href="#">Action</a></li>
