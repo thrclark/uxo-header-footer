1f28dcdb8b5c80c848f935430cb3dd4cbd6838c4
thrclark <thrclark@toms-mbp.attlocal.net>
2016-09-27 12:30:52 -0400

    sign in link

--- php/includes/pub-header.php
+++ php/includes/pub-header.php
@@ -1,14 +1,23 @@
-<header style="min-height:115px">
+<header style="min-height:100px">
     <div class="container">
         <div class="row">
             <div class="col-md-3">
-                <h1 class="navbar-brand" style="font-size: 44px;
-width: 100%;
-margin-top: 10px;">IU Verify</h1>
-                <div class="small" style="color:#ffffff">Publishing & Administration</div>
+                <h1 class="navbar-brand" style="font-size: 44px;width: 100%;margin-top: 10px;">IU Verify</h1>
+                <div class="small" style="color:#ffffff">Verification Publishing</div>
             </div>
-            <div class="col-md-9">
-                <p class="navbar-text" style="color:#E4E4E4;margin-top: 26px;"><i class="fa fa-user"></i> <span class="sr-only">User: </span>thrclark</p>
+            <div class="col-md-9"> 
+                <!-- <p class="navbar-text" style="color:#E4E4E4;margin-top: 26px;"><i class="fa fa-user"></i> <span class="sr-only">User: </span>thrclark</p>-->
+                <div class="dropdown navbar-text">
+                    <button id="dLabel" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"  class="btn btn-link" style="color:#e4e4e4" >Hello, Tom <span class="caret"></span> </button>
+                    <ul class="dropdown-menu" aria-labelledby="dLabel">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Action</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Sign Out</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
