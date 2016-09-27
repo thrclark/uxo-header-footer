a2c1280f0f433c75e5b5c24607e2cdccafa47685
thrclark <thrclark@toms-mbp.attlocal.net>
2016-09-27 10:46:08 -0400

    Publishing menu fix

--- php/includes/pub-nav.php
+++ php/includes/pub-nav.php
@@ -3,13 +3,13 @@
         <div class="menu-list">
             <ul id="menu-content" class="menu-content collapse out">
                 <li  data-toggle="collapse" data-target="#products" class="collapsed active"><a href="#">Publishing <span class="arrow"></span></a></li>
-                <ul class="sub-menu" id="products">
+                <ul <?php if ($section == 'publishing') {echo 'class="sub-menu"';} else {echo 'class="sub-menu collapse"';} ?> id="products">
                     <li <?php if ($page_title == 'home') { echo 'class="active"'; } ?>><a href="vfy-pub-home.php">View All Verifications</a></li>
                     <li <?php if ($page_title == 'create-new') { echo 'class="active"'; } ?>><a href="vfy-pub-new-verification.php">Create New Verification</a></li>
                     <li <?php if ($page_title == 'archived') { echo 'class="active"'; } ?>><a href="vfy-pub-archived.php">Archived Verifications</a></li>
                 </ul>
                 <li data-toggle="collapse" data-target="#service" class="collapsed"><a href="#">Settings <span class="arrow"></span></a></li>
-                <ul class="sub-menu collapse" id="service">
+                <ul <?php if ($section == 'settings') {echo 'class="sub-menu"';} else {echo 'class="sub-menu collapse"';} ?>id="service">
                     <li <?php if ($page_title == 'general-settings') { echo 'class="active"'; } ?>><a href="vfy-pub-settings.php">General Settings</a></li>
                     <li <?php if ($page_title == 'api-accounts') { echo 'class="active"'; } ?>><a href="vfy-pub-api-accounts-home.php">API Accounts </a></li>
                 </ul>
