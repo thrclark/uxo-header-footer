a155b2158efc8be04ed33c8bc9d4675f19a4e3f5
thrclark <thrclark@Toms-MacBook-Pro.local>
2016-10-20 06:52:01 -0400

    Added ldap page

---
+++ php/vfy-pub-ldap.php
@@ -0,0 +1,42 @@
+<?php 
+$section = 'settings';
+$page_title = 'ldap';
+?>
+<!DOCTYPE html>
+<html lang=en-us>
+<head>
+<?php include ('includes/head-meta.php') ?>
+<?php include ('includes/styles-publishing.php') ?>
+<?php include ('includes/scripts.php') ?>
+<!-- Responsive Tables-->
+<script type="text/javascript" src="../js/tablesaw.js"></script>
+<script type="text/javascript" src="../js/tablesaw-init.js"></script>
+</head>
+<body class="view-admin" id="top">
+<div class="wrapper">
+    <?php include ('includes/brand-header2.php') ?>
+    <?php include ('includes/pub-header.php') ?>
+    <div class="main-content container" role=main>
+        <div id="main-content">
+            <div class="row">
+                <div class="col-md-3">
+                    <?php include ('includes/pub-nav.php') ?>
+                </div>
+                <div class="col-md-9">
+                    <div class="primary-content">
+                        <section>
+                            <div class="row section-head">
+                                <div class="col-sm-12 section-header">
+                                    <h2 class="h3">LDAP Configuration</h2>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include ('includes/brand-footer.php') ?>
+</div>
+</body>
+</html>
