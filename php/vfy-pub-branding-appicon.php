de5afb77a081972ae7cbf9566a18b9fb6b8e8dcd
Tom Clark <thrclark@iu.edu>
2016-12-14 16:37:52 -0500

    App icon publishing

--- php/vfy-pub-branding-appicon.php
+++ php/vfy-pub-branding-appicon.php
@@ -13,14 +13,8 @@ $page_title = 'app-icon';
 }
 </style>
 <?php include ('includes/scripts.php') ?>
-
 <link  href="../node_modules/cropper/dist/cropper.css" rel="stylesheet" type="text/css">
 <script src="../node_modules/cropper/dist/cropper.js"></script>
-
-
-
-
-
 </head>
 <body class="view-admin" id="top">
 <div class="wrapper">
@@ -34,19 +28,75 @@ $page_title = 'app-icon';
                 </div>
                 <div class="col-md-9">
                     <div class="primary-content">
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    <div>
-  <img id="image" src="../img/picture.jpg">
-</div><br><br><br><br><br><br>
+                        <div class="container" id="crop-avatar"> 
+                            
+                            <!-- Current avatar -->
+                            <div class="avatar-view" title="Change the avatar"> <img src="img/appicon.png" alt="Avatar"> </div>
+                            
+                            <!-- Cropping modal -->
+                            <div class="modal fade" id="avatar-modal" aria-hidden="true" aria-labelledby="avatar-modal-label" role="dialog" tabindex="-1">
+                                <div class="modal-dialog modal-default modal-lg">
+                                    <div class="modal-content">
+                                        <form class="avatar-form" action="crop.php" enctype="multipart/form-data" method="post">
+                                            <div class="modal-header">
+                                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                                <h4 class="modal-title" id="avatar-modal-label">Change Avatar</h4>
+                                            </div>
+                                            <div class="modal-body">
+                                                <div class="avatar-body"> 
+                                                    
+                                                    <!-- Upload image and data -->
+                                                    <div class="avatar-upload">
+                                                        <input type="hidden" class="avatar-src" name="avatar_src">
+                                                        <input type="hidden" class="avatar-data" name="avatar_data">
+                                                        <label for="avatarInput">Local upload</label>
+                                                        <input type="file" class="avatar-input" id="avatarInput" name="avatar_file">
+                                                    </div>
+                                                    
+                                                    <!-- Crop and preview -->
+                                                    <div class="row">
+                                                        <div class="col-md-9">
+                                                            <div class="avatar-wrapper"></div>
+                                                        </div>
+                                                        <div class="col-md-3">
+                                                            <div class="avatar-preview preview-lg"></div>
+                                                            <div class="avatar-preview preview-md"></div>
+                                                            <div class="avatar-preview preview-sm"></div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row avatar-btns">
+                                                        <div class="col-md-9">
+                                                            <div class="btn-group">
+                                                                <button type="button" class="btn btn-primary" data-method="rotate" data-option="-15" title="Rotate -90 degrees">Rotate Left</button>
+                                                            </div>
+                                                            <div class="btn-group">
+                                                                <button type="button" class="btn btn-primary" data-method="rotate" data-option="15" title="Rotate 90 degrees">Rotate Right</button>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-3">
+                                                            <button type="submit" class="btn btn-primary btn-block avatar-save">Done</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <!-- <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div> -->
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /.modal --> 
+                            
+                            <!-- Loading state -->
+                            <div class="loading" aria-label="Loading" role="img" tabindex="-1"></div>
+                        </div>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
                         <section>
                             <div class="row section-head">
                                 <div class="col-sm-12 section-header">
@@ -61,17 +111,9 @@ $page_title = 'app-icon';
                             </ul>
                             <div class="tab-content">
                                 <div id="menu1" class="tab-pane fade in active">
-                                
-                                
-                                <fieldset>
-                                
-                                <legend>Favicon</legend>
-                                
-                                </fieldset>
-                                
-                                
-                                
-                                    
+                                    <fieldset>
+                                        <legend>Favicon</legend>
+                                    </fieldset>
                                 </div>
                                 <div id="menu2" class="tab-pane fade">
                                     <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
@@ -131,23 +173,7 @@ $page_title = 'app-icon';
         </div>
     </div>
 </div>
-
-
-
-<script>
-$('#image').cropper({
-  aspectRatio: 1 / 1,
-  crop: function(e) {
-    // Output the result data for cropping image.
-    console.log(e.x);
-    console.log(e.y);
-    console.log(e.width);
-    console.log(e.height);
-    console.log(e.rotate);
-    console.log(e.scaleX);
-    console.log(e.scaleY);
-  }
-});
-</script>
+<script src="../js/cropper-main.js"></script> 
+å
 </body>
 </html>
