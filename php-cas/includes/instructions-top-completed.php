637f66256248a6c30f2ea5806592259d02c6bc6e
Tom Clark <thrclark@iu.edu>
2017-03-08 10:13:51 -0500

    cleanup

--- php-cas/includes/instructions-top-completed.php
+++
@@ -1,8 +0,0 @@
-<hr>
-<div class="row">
-    <div class="col-md-12"> 
-        <!-- <h2 class="h3" style="margin-top:0px">Verifcation Complete</h2>-->
-        <p class="lead"> There are no more items requiring your verification at this time. Thank you.</p>
-    </div>
-</div>
-<hr>
--- php-cas/includes/instructions-top.php
+++
@@ -1,8 +0,0 @@
-<div class="row">
-    <div class="col-md-12">
-        <h2 class="h4" style="margin-top:0px">Verify Your Data</h2>
-        <p> On occasion, Indiana University requires verification of personal information regarding your affiliation with the university. The item(s) presented on this page have been marked as needing your review for accuracy. </p>
-        <p>This is an additional, optional paragraph that may be customized as needed. This paragraph can include <a href="#">links</a> that could take the user to help content, or <a href="#">contact information</a>. </p>
-    </div>
-</div>
-<hr>
